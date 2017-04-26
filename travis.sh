b5979bcc0bc07048ffdd3de0b2437697a116dbdb
Karsten Kraus <Karsten.Kraus@swr.de>
2017-04-26 18:42:17 +0200

    debugging...

--- travis.sh
+++ travis.sh
@@ -8,7 +8,7 @@ echo "Building branch $TRAVIS_BRANCH (pull-request: $TRAVIS_PULL_REQUEST)..."
 #if [ "$TRAVIS_TAG" != "" ]; then
     echo "Publishing release $TRAVIS_TAG..."
     
-    ./gradlew publish -Prelease=${TRAVIS_TAG}
+#    ./gradlew publish -Prelease=${TRAVIS_TAG}
 
     REPO=`git config remote.origin.url`
     ORIGIN=${REPO/https:\/\/github.com/https://${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}@github.com}
