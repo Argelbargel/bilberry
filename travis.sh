d02a8792a1c31c788c81bccce4d16829e40bdfac
Karsten Kraus <Karsten.Kraus@swr.de>
2017-04-26 16:06:35 +0200

    simplified build script

--- travis.sh
+++ travis.sh
@@ -5,29 +5,29 @@ echo "Building branch $TRAVIS_BRANCH (pull-request: $TRAVIS_PULL_REQUEST)..."
 ./gradlew check -Prelease=${TRAVIS_TAG}
 
 if [ "$TRAVIS_TAG" != "" ]; then
+    echo "Publishing release $TRAVIS_TAG..."
+    
     ./gradlew publish -Prelease=${TRAVIS_TAG}
 
-    if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-        REPO=`git config remote.origin.url`
-        TARGET_REPO=${REPO/https:\/\/github.com/https://${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}@github.com}
-        TARGET_BRANCH="mvn-repo"
+    REPO=`git config remote.origin.url`
+    TARGET_REPO=${REPO/https:\/\/github.com/https://${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}@github.com}
+    TARGET_BRANCH="mvn-repo"
 
-        git clone $REPO out --no-checkout
-        cd out
-        git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-        git config user.name "Travis CI"
-        git config user.email "$COMMIT_AUTHOR_EMAIL"
+    git clone $REPO build/deploy --no-checkout
+    cd build/deploy
+    git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+    git config user.name "Travis CI"
+    git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-        cp -r ../build/repo/* .
+    cp -r ../repo/* .
 
-        if [ -z `git diff --exit-code` ]; then
-            echo "No changes to the output on this push; exiting."
-            exit 0
-        fi
-
-        git add .
-        git commit -m "Deploy release : $TRAVIS_TAG"
-        git push $TARGET_REPO $TARGET_BRANCH
+    if [ -z `git diff --exit-code` ]; then
+        echo "No changes to the output on this push; exiting."
+        exit 0
     fi
+
+    git add .
+    git commit -m "Deploy release : $TRAVIS_TAG"
+    git push $TARGET_REPO $TARGET_BRANCH
 fi
 
