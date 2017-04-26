c7834145b6864a4078b2d2bce201ce6785a5288d
Karsten Kraus <Karsten.Kraus@swr.de>
2017-04-26 19:53:09 +0200

    key trouble...

--- travis.sh
+++ travis.sh
@@ -34,11 +34,14 @@ echo "Building branch $TRAVIS_BRANCH (pull-request: $TRAVIS_PULL_REQUEST)..."
         git add .
         git commit -m "release: $TRAVIS_TAG"
 
+        echo "Decrypting deploy_key..."
         ENCRYPTED_KEY_VAR="encrypted_${DEPLOY_KEY_ID}_key"
         ENCRYPTED_IV_VAR="encrypted_${DEPLOY_KEY_ID}_iv"
         ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
         ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
         openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ../../deploy_key.enc -out deploy_key -d
+
+        echo "Adding decrypted key..."
         chmod 600 deploy_key
         eval `ssh-agent -s`
         ssh-add deploy_key
