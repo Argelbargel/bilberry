d2387a4891c671428823c66a86a0b03fb6ab9380
Karsten Kraus <Karsten.Kraus@swr.de>
2017-04-26 15:27:35 +0200

    add publication to mvn-repo-branch

--- travis.sh
+++ travis.sh
@@ -5,5 +5,28 @@
 
 if [ "$TRAVIS_TAG" != "" ]; then
     ./gradlew publish -Prelease=${TRAVIS_TAG}
+
+    if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]; then
+        REPO=`git config remote.origin.url`
+        TARGET_REPO=${REPO/https:\/\/github.com/https://${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}@github.com}
+        TARGET_BRANCH="mvn-repo"
+
+        git clone $REPO out --no-checkout
+        cd out
+        git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+        git config user.name "Travis CI"
+        git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+        cp -r ../build/repo/* .
+
+        if [ -z `git diff --exit-code` ]; then
+            echo "No changes to the output on this push; exiting."
+            exit 0
+        fi
+
+        git add .
+        git commit -m "Deploy release : $TRAVIS_TAG"
+        git push $TARGET_REPO $TARGET_BRANCH
+    fi
 fi
 
